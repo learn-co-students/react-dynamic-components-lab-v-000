cf71be98c14b2200abc3d344ec231ddabe5086ca
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-07-05 12:08:39 -0400

    updating solution

--- src/Comment.js
+++ src/Comment.js
@@ -1,12 +1,7 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
 
 export default class Comment extends Component {
-  render() {
-    return (
-      <div className="comment">
-        {this.props.commentText}
-      </div>
-    )
-  }
+	render() {
+		return <div className="comment">{this.props.commentText}</div>;
+	}
 }

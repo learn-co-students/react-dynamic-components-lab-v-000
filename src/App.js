862ba085d60ef1fc8aa9c536993e56a78ea13376
JGBrown <j.grant.brown@gmail.com>
2018-08-14 12:45:07 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import BlogPost from './BlogPost.js'
 import ColorBox from './ColorBox.js'
 
+
 class App extends Component {
-  
-  // Nothing needs to change here! 
+
+  // Nothing needs to change here!
   // Make note of what prop we are initially passing to `ColorBox` below!
-  
+
   render() {
     return (
       <div id="app">

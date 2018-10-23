bd275ff7b36f327cb42af0d392a00d6a1885f692
Su0414 <susanaik.com>
2018-10-23 10:35:58 -0700

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
   
-  render() {
-    return (
-      <div className="color-box" style={{opacity: 2}}>
-        {/* your conditional code here! */}
-      </div>
-    )
+  render() {   
+        
+        const newValue = this.props.opacity - 0.1;
+ 
+        if (this.props.opacity >= 0.2) {
+            return (
+              <div className="color-box" style={{opacity: this.props.opacity}}>
+                  <ColorBox opacity={newValue} />
+              </div>
+            );
+        } else {
+            return null;
+        }
+     
+    
   }
   
 }

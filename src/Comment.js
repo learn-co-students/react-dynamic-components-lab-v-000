6710fa0d0183b716688234d1d83b37508af9c0c0
AubreyNeimeier <neimeier.1@osu.edu>
2018-11-05 20:56:22 -0800

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,16 @@
 // add Comment component here
+// eslint-disable-next-line
+import React, { Component } from 'react';
+
+class Comment extends React.Component {
+    render(){
+        return (
+            <div className="comment">
+            {this.props.commentText}
+            </div>
+        )
+    }
+}
+
+
+export default Comment;

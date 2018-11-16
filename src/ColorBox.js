838bdc9d2c943564aa34d9e188de4b0bfc889da8
sgibson47 <sgibson47@gmail.com>
2018-11-16 14:11:10 -0600

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
-  
+
   render() {
+    let parentOpacity = this.props.opacity;
+    function disappearingColorBoxes(){
+      if (parentOpacity >= .2){
+        return <ColorBox opacity={(parentOpacity - .1)} />
+      }
+    }
+
     return (
-      <div className="color-box" style={{opacity: 2}}>
-        {/* your conditional code here! */}
+      <div className="color-box" style={{opacity: this.props.opacity}}>
+        {disappearingColorBoxes()}
       </div>
     )
   }
-  
 }

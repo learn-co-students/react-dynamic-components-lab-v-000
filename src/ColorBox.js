bbbaf1dd86d228e76933d93187ff8275857445c3
marynavoitenko <maryna.voitenko@gmail.com>
2018-12-30 13:46:46 -0500

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 export default class ColorBox extends Component {
-  
+
   render() {
+    const { opacity } = this.props;
+
     return (
-      <div className="color-box" style={{opacity: 2}}>
-        {/* your conditional code here! */}
+      <div className="color-box" style={{opacity: opacity}}>
+        { opacity >= 0.2 ? <ColorBox opacity={opacity - 0.1} /> : null }
       </div>
     )
   }
-  
+
 }
--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,15 @@
-// add Comment component here
+import React, { Component } from 'react';
+
+class Comment extends Component {
+  render() {
+    const { commentText } = this.props;
+    
+    return (
+      <div className="comment">
+        {commentText}
+      </div>
+    )
+  }
+}
+
+export default Comment;

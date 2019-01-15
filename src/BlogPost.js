5c3f0efce36d2036e495e411aa865ce58eef09ed
github username <github email address>
2019-01-14 23:02:19 -0800

    Remove ColorBox

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -27,7 +27,6 @@ export default class BlogPost extends Component {
         <Comment commentText={comments[0]} />
         <Comment commentText={comments[1]} />
         <Comment commentText={comments[2]} />
-        <ColorBox />
       </div>
     );
   }

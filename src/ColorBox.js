a22b6a2741e37039dc742893ab6405d40ba2e47e
github username <github email address>
2019-01-14 23:01:58 -0800

    Remove originalVal and switch with this.props.opacity; change if condition to >= 0.2

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -6,16 +6,15 @@ export default class ColorBox extends Component {
   render() {
     var precise = x => Number.parseFloat(x).toPrecision(1);
     let renderColorBox;
-    const originalVal = +(precise(this.props.opacity - 0))
     const newVal = +(precise(this.props.opacity - 0.1))
 
-    if (this.props.opacity > 0.2) {
+    if (this.props.opacity >= 0.2) {
       renderColorBox = <ColorBox opacity={newVal} />
     } else {
       renderColorBox = null
     }
     return (
-      <div className="color-box" style={{opacity: originalVal}}>
+      <div className="color-box" style={{opacity: this.props.opacity}}>
         { renderColorBox }
       </div>
     )

cce46d1299ce488f36749dfcf724c61e3c6632fc
Kkelly22 <kk504408@gmail.com>
2019-03-14 18:37:16 -0400

    completed tests

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// are we missing an import?
+import Comment from './Comment.js';
 
 export default class BlogPost extends Component {
   render() {
@@ -16,14 +16,14 @@ export default class BlogPost extends Component {
         Come at me<br/>
         I have been preparing<br/>
         My meme fortress is built<br/>
-        I won't let you tilt me<br/>
+        I wont let you tilt me<br/>
         You only grew accustomed to the salt<br/>
         I was born in it<br/>
-        {/* (remove the comment ticks and their surrounding brackets)
+        
         <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        */}
+        <Comment commentText={comments[1]} />
+        <Comment commentText={comments[2]} />
+   
       </div>
     );
   }

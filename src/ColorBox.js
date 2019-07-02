84f467a966bf32f2befee3b0d1489d0d5f37b77c
Elizabeth Karst <elizabeth.karst@viget.com>
2019-07-02 12:13:07 -0600

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -18,28 +18,3 @@ export default class ColorBox extends Component {
     }
   }
 }
-
-
-// class Example extends Component {
-//   render() {
-//       const newValue = this.props.value * 2;
-
-//       if (this.props.value < 100) {
-//           return (
-//               <div>
-//                   <Example value={newValue} />
-//               </div>
-//           );
-//       } else {
-//           return null;
-//       }
-//   }
-// }
-
-// if the opacity value is greater than or equal to 0.2:
-// the ColorBox component should render another ColorBox itself (recursive components!)
-// an opacity prop should be passed to the child
-// the passed opacity prop should be reduced by 0.1
-// if the opacity value is less than 0.2:
-// do not render another ColorBox (or else we would have infinite ColorBoxes rendering!)
-// instead, render null

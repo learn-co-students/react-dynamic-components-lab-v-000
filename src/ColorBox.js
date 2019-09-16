aefe698b6e1d0945e3e893c16b9fc7df24b5784d
Madeline-Stark <madeline.stark1@gmail.com>
2019-09-16 13:21:29 -0700

    Update ColorBox.js

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -15,3 +15,17 @@ export default class ColorBox extends Component {
       }
     }
 }
+
+//or with ternary:
+// import React, { Component } from "react";
+
+// export default class ColorBox extends Component {
+//   render() {
+//     let newOpacity = this.props.opacity - 0.1;
+//     return (
+//       <div className="color-box" style={{ opacity: this.props.opacity }}>
+//         {this.props.opacity >= 0.2 ? <ColorBox opacity={newOpacity} /> : null}
+//       </div>
+//     );
+//   }
+// }

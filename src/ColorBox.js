e1b7ef374b6c39bc85aa067d87c494a161834e1d
rgrier104 <rgrier104@gmail.com>
2019-12-05 14:56:26 -1000

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 export default class ColorBox extends Component {
 
   render() {
-    return (
-      <div className="color-box" style={{opacity: 2}}>
-        {/* your conditional code here! */}
-      </div>
-    )
+    console.log(this.props.opacity)
+    let newOpacity = (Math.round(this.props.opacity * 10) / 10) - .1;
+    {
+      return this.props.opacity <= .1 ? null : (
+        <div className="color-box" style={{ opacity: this.props.opacity }}>
+          <ColorBox opacity={newOpacity} />
+        </div>
+      )
+    }
   }
 
 }
--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,14 @@
 //your code here
+
+import React, { Component } from 'react';
+// import BlogPost from './BlogPost';
+
+export default class Comment extends Component {
+    render() {
+        return (
+            <div className="comment">
+                {this.props.commentText}
+            </div>
+        )
+    }
+}

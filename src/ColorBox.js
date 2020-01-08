68cff19ecbb81566c540f6ae319806f846563eb5
John Blank <veloblank@gmail.com>
2020-01-08 06:31:12 -0500

    Make style changes. Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 
 class ColorBox extends Component {
-	render() {
-		return (
-			<div className="color-box" style={{ opacity: 1 }}>
-				{this.props.opacity >= 0.2 ? (
-					<ColorBox opacity={this.props.opacity - 0.1} />
-				) : null}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="color-box" style={{ opacity: this.props.opacity }}>
+        {this.props.opacity >= 0.2 ? (
+          <ColorBox opacity={this.props.opacity - 0.15} />
+        ) : null}
+      </div>
+    );
+  }
 }
 export default ColorBox;

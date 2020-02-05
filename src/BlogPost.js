5b7ab1f63129ce24086c016a359b610ebf4767e0
sylwiavargas <sylwia.vargas@gmail.com>
2020-02-05 11:54:48 -0500

    update blogpost

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -18,9 +18,6 @@ export default class BlogPost extends Component {
         Still I'll rise.<br/>
         -Maya Angelou<br/>
 
-        {/* (remove the comment ticks and their surrounding brackets)
-        <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
         <Comment commentText={comments[0]} />
         <Comment commentText={comments[1]} />
         <Comment commentText={comments[2]} />

3dde27f77b2e69511048121a9637e7317972e011
ahotchkin <allyson.hotchkin@gmail.com>
2020-05-15 14:48:23 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import BlogPost from './BlogPost.js'
 import ColorBox from './ColorBox.js'
 
 class App extends Component {
-  
-  // Nothing needs to change here! 
+
+  // Nothing needs to change here!
   // Make note of what prop we are initially passing to `ColorBox` below!
-  
+
   render() {
     return (
       <div id="app">
-          <BlogPost />
-          <div id="seperator"></div>
-          <div className="wrapper">
-            <ColorBox opacity={1} />
-          </div>
+        <BlogPost />
+        <div id="seperator"></div>
+        <div className="wrapper">
+          <ColorBox opacity={1} />
+        </div>
       </div>
     )
   }

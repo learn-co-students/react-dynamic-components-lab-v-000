cee882193c94a19edff5dccdcee83fa24ce985f2
azharshahstl <azharshahstl@yahoo.com>
2020-07-20 11:14:57 -0500

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -9,19 +9,14 @@ export default class ColorBox extends Component {
   }
 
   render() {
-    if (this.props.value - 0.1) {
-    return ( <div className="color-box" style={{opacity: this.props.opacity}}>
-        <ColorBox opacity={this.props.opacity - 0.1} />
-        </div>)
-    } 
-    else {
+    if(this.props.opacity >= 0.2) {
+      return (
+        <div className="color-box" style={{opacity: this.props.opacity}}>
+          <ColorBox opacity={this.props.opacity - 0.1} />
+        </div>
+      )
+    } else {
       return null
     }
   }
-
 }
-
-// const newValue = this.props.value - 0.1;
-//     return this.props.value < 0.2 ? null : ( <div>
-//         <Example opacity={newValue} />
-//         </div>)
--- src/Comment.js
+++ src/Comment.js
@@ -1,4 +1,3 @@
-//your code here
 import React, { Component } from 'react'
 
 export default class Comment extends Component {

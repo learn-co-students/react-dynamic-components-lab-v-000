e3fc1eceb3441731e8571827701a0aec4d11fcc2
Olivia Tufte <ortufte@gmail.com>
2020-09-09 16:05:16 -0500

    Done.

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -19,8 +19,8 @@ export default class BlogPost extends Component {
         -Maya Angelou<br/>
 
         <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
-        <Comment commentText={comments[0]} />
+        <Comment commentText={comments[1]} />
+        <Comment commentText={comments[2]} />
    
       </div>
     );
--- src/Comment.js
+++ src/Comment.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Comment extends Component {
     render() {
         return (
-            <div class="comment">
+            <div className="comment">
                 {this.props.commentText}
             </div>
         )

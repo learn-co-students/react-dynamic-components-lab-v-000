24f4afc089822b07625edbc73c683a82287f9010
Eric Johnson <ericjohnson0216@icloud.com>
2020-12-27 19:06:17 -0500

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -11,7 +11,7 @@ export default class ColorBox extends Component {
   render() {
     return (
       <div className="color-box" style={{opacity: this.props.opacity /*replace null with the value*/}}>
-          
+         
       </div>
     )
   }
